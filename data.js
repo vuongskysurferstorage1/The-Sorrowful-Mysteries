c4be82b7b15b38fec42653ebd12bbed26175f056
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-11 04:18:53 -0500

    update

---
+++ data.js
@@ -0,0 +1,267 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-the-agony-in-the-garden",
+      "name": "1. The Agony in the Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7122755380944525,
+          "pitch": -0.022680526569820003,
+          "title": "test",
+          "text": "test"
+        }
+      ]
+    },
+    {
+      "id": "1-2-the-scourging-at-the-pillar",
+      "name": "2. The Scourging at the Pillar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03732539266628443,
+          "pitch": -0.10001596075944796,
+          "rotation": 0,
+          "target": "2-3-crowning-with-thorns"
+        },
+        {
+          "yaw": -2.352292590955379,
+          "pitch": -0.053396114413834184,
+          "rotation": 0,
+          "target": "0-1-the-agony-in-the-garden"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.01273968221872579,
+          "pitch": -0.42824653428044357,
+          "title": "test",
+          "text": "test"
+        }
+      ]
+    },
+    {
+      "id": "2-3-crowning-with-thorns",
+      "name": "3. Crowning with Thorns",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7806589727883413,
+          "pitch": -0.07899997168400219,
+          "rotation": 0,
+          "target": "3-4-the-carrying-of-the-cross"
+        },
+        {
+          "yaw": 3.1209863822329336,
+          "pitch": -0.12828587764548516,
+          "rotation": 0,
+          "target": "1-2-the-scourging-at-the-pillar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8007732690984959,
+          "pitch": -0.4699202557136992,
+          "title": "test",
+          "text": "test"
+        }
+      ]
+    },
+    {
+      "id": "3-4-the-carrying-of-the-cross",
+      "name": "4. The Carrying of the Cross",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9907525571087445,
+          "pitch": -0.43353448405620476,
+          "rotation": 0,
+          "target": "4-5-the-crucifixion-of-jesus"
+        },
+        {
+          "yaw": -1.9497772696554847,
+          "pitch": 0.30493260944028755,
+          "rotation": 0,
+          "target": "2-3-crowning-with-thorns"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.94247417162436,
+          "pitch": -0.6808765671709516,
+          "title": "test",
+          "text": "test"
+        }
+      ]
+    },
+    {
+      "id": "4-5-the-crucifixion-of-jesus",
+      "name": "5. The Crucifixion of Jesus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8831040737467255,
+          "pitch": 0.37172238316890116,
+          "rotation": 0,
+          "target": "3-4-the-carrying-of-the-cross"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6673373618952212,
+          "pitch": -0.658001776742676,
+          "title": "test",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
