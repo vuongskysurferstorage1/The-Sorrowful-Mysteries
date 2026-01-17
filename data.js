b3bc1c947a09e1fc2382fe029c75ec67e1e56257
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-17 01:33:10 -0500

    update

--- data.js
+++ data.js
@@ -34,18 +34,18 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.8159711529622591,
-          "pitch": 0.16363445477363747,
+          "yaw": 0.8531931759373297,
+          "pitch": 0.18269359781554506,
           "rotation": 0,
           "target": "1-2-the-scourging-at-the-pillar"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.7673077908718007,
-          "pitch": -0.06791063844870138,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 0.7900758344091656,
+          "pitch": -0.04940067764988321,
+          "title": "Gethsemane",
+          "text": "Then Jesus went with his disciples to a place called Gethsemane, and he said to them, “Sit here while I go over there and pray.” He took Peter and the two sons of Zebedee along with him, and he began to be sorrowful and troubled. Then he said to them, “My soul is overwhelmed with sorrow to the point of death. Stay here and keep watch with me.”<div><br></div><div>Going a little farther, he fell with his face to the ground and prayed, “My Father, if it is possible, may this cup be taken from me. Yet not as I will, but as you will.”</div><div><br></div><div>Then he returned to his disciples and found them sleeping. “Couldn’t you men keep watch with me for one hour?” he asked Peter. “Watch and pray so that you will not fall into temptation. The spirit is willing, but the flesh is weak.”</div><div><br></div><div>He went away a second time and prayed, “My Father, if it is not possible for this cup to be taken away unless I drink it, may your will be done.”</div><div><br></div><div>When he came back, he again found them sleeping, because their eyes were heavy. So he left them and went away once more and prayed the third time, saying the same thing.</div><div><br></div><div>Then he returned to the disciples and said to them, “Are you still sleeping and resting? Look, the hour has come, and the Son of Man is delivered into the hands of sinners. Rise! Let us go! Here comes my betrayer!”</div><div>(Matthew 26:36-46)</div>"
         }
       ]
     },
@@ -87,20 +87,14 @@ var APP_DATA = {
           "pitch": -0.11489771403726756,
           "rotation": 0,
           "target": "2-3-crowning-with-thorns"
-        },
-        {
-          "yaw": -2.35895181494695,
-          "pitch": -0.10103915462782886,
-          "rotation": 0,
-          "target": "0-1-the-agony-in-the-garden"
         }
       ],
       "infoHotspots": [
         {
           "yaw": -0.019879467823962216,
           "pitch": -0.3777665840187243,
-          "title": "Title",
-          "text": "Text"
+          "title": "Jesus Before Pilate",
+          "text": "Very early in the morning, the chief priests, with the elders, the teachers of the law and the whole Sanhedrin, made their plans. So they bound Jesus, led him away and handed him over to Pilate.<div><br></div><div>“Are you the king of the Jews?” asked Pilate.</div><div><br></div><div>“You have said so,” Jesus replied.</div><div><br></div><div>The chief priests accused him of many things. So again Pilate asked him, “Aren’t you going to answer? See how many things they are accusing you of.”</div><div><br></div><div>But Jesus still made no reply, and Pilate was amazed.</div><div><br></div><div>Now it was the custom at the festival to release a prisoner whom the people requested. A man called Barabbas was in prison with the insurrectionists who had committed murder in the uprising. The crowd came up and asked Pilate to do for them what he usually did.</div><div><br></div><div>“Do you want me to release to you the king of the Jews?” asked Pilate, 10 knowing it was out of self-interest that the chief priests had handed Jesus over to him. 11 But the chief priests stirred up the crowd to have Pilate release Barabbas instead.</div><div><br></div><div>“What shall I do, then, with the one you call the king of the Jews?” Pilate asked them.</div><div><br></div><div>“Crucify him!” they shouted.</div><div><br></div><div>“Why? What crime has he committed?” asked Pilate.</div><div><br></div><div>But they shouted all the louder, “Crucify him!”</div><div><br></div><div>Wanting to satisfy the crowd, Pilate released Barabbas to them. He had Jesus flogged, and handed him over to be crucified.</div><div>(Mark 15:1-15)</div>"
         }
       ]
     },
@@ -154,8 +148,8 @@ var APP_DATA = {
         {
           "yaw": -0.7715165002859088,
           "pitch": -0.2086324739733545,
-          "title": "Title",
-          "text": "Text"
+          "title": "The Soldiers Mock Jesus",
+          "text": "The soldiers led Jesus away into the palace (that is, the Praetorium) and called together the whole company of soldiers. They put a purple robe on him, then twisted together a crown of thorns and set it on him. And they began to call out to him, “Hail, king of the Jews!” Again and again they struck him on the head with a staff and spit on him. Falling on their knees, they paid homage to him. And when they had mocked him, they took off the purple robe and put his own clothes on him. Then they led him out to crucify him.<div>(Mark 15:16-19)</div>"
         }
       ]
     },
@@ -209,8 +203,8 @@ var APP_DATA = {
         {
           "yaw": 0.6753313773404095,
           "pitch": -0.6087821712042061,
-          "title": "Title",
-          "text": "Text"
+          "title": "Title - Confirm",
+          "text": "As the soldiers led him away, they seized Simon from Cyrene, who was on his way in from the country, and put the cross on him and made him carry it behind Jesus. A large number of people followed him, including women who mourned and wailed for him. Jesus turned and said to them, “Daughters of Jerusalem, do not weep for me; weep for yourselves and for your children. For the time will come when you will say, ‘Blessed are the childless women, the wombs that never bore and the breasts that never nursed!’ Then<div><br></div><div>“‘they will say to the mountains, “Fall on us!” and to the hills, “Cover us!”’</div><div><br></div><div>For if people do these things when the tree is green, what will happen when it is dry?”</div><div>(Luke 23:26-31)</div>"
         }
       ]
     },
