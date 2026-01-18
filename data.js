374ae1971ed1025e639a657f39f2f972021e05c0
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-17 23:20:04 -0500

    update

--- data.js
+++ data.js
@@ -28,24 +28,24 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.4772376328494179,
+        "pitch": 0.12355282602080031,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [
         {
-          "yaw": 0.8531931759373297,
-          "pitch": 0.18269359781554506,
+          "yaw": 0.8140163423656261,
+          "pitch": 0.18800449154947252,
           "rotation": 0,
           "target": "1-2-the-scourging-at-the-pillar"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.7900758344091656,
-          "pitch": -0.04940067764988321,
+          "yaw": 0.6649123632899396,
+          "pitch": -0.07450796971354023,
           "title": "Gethsemane",
-          "text": "Then Jesus went with his disciples to a place called Gethsemane, and he said to them, “Sit here while I go over there and pray.” He took Peter and the two sons of Zebedee along with him, and he began to be sorrowful and troubled. Then he said to them, “My soul is overwhelmed with sorrow to the point of death. Stay here and keep watch with me.”<div><br></div><div>Going a little farther, he fell with his face to the ground and prayed, “My Father, if it is possible, may this cup be taken from me. Yet not as I will, but as you will.”</div><div><br></div><div>Then he returned to his disciples and found them sleeping. “Couldn’t you men keep watch with me for one hour?” he asked Peter. “Watch and pray so that you will not fall into temptation. The spirit is willing, but the flesh is weak.”</div><div><br></div><div>He went away a second time and prayed, “My Father, if it is not possible for this cup to be taken away unless I drink it, may your will be done.”</div><div><br></div><div>When he came back, he again found them sleeping, because their eyes were heavy. So he left them and went away once more and prayed the third time, saying the same thing.</div><div><br></div><div>Then he returned to the disciples and said to them, “Are you still sleeping and resting? Look, the hour has come, and the Son of Man is delivered into the hands of sinners. Rise! Let us go! Here comes my betrayer!”</div><div>(Matthew 26:36-46)</div>"
+          "text": "Then Jesus went with his disciples to a place called Gethsemane, and he said to them, “Sit here while I go over there and pray.” He took Peter and the two sons of Zebedee along with him, and he began to be sorrowful and troubled. Then he said to them, “My soul is overwhelmed with sorrow to the point of death. Stay here and keep watch with me.”<div><br></div><div>Going a little farther, he fell with his face to the ground and prayed, “My Father, if it is possible, may this cup be taken from me. Yet not as I will, but as you will.”</div><div><br></div><div>Then he returned to his disciples and found them sleeping. “Couldn’t you men keep watch with me for one hour?” he asked Peter. “Watch and pray so that you will not fall into temptation. The spirit is willing, but the flesh is weak.”</div><div><br></div><div>He went away a second time and prayed, “My Father, if it is not possible for this cup to be taken away unless I drink it, may your will be done.”</div><div><br></div><div>When he came back, he again found them sleeping, because their eyes were heavy. So he left them and went away once more and prayed the third time, saying the same thing.</div><div><br></div><div>Then he returned to the disciples and said to them, “Are you still sleeping and resting? Look, the hour has come, and the Son of Man is delivered into the hands of sinners. Rise! Let us go! Here comes my betrayer!”&nbsp;</div><div>(Matthew 26:36-46)</div>"
         }
       ]
     },
@@ -83,18 +83,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.033296389592090136,
-          "pitch": -0.11489771403726756,
+          "yaw": 0.03629086047699204,
+          "pitch": -0.12715067557087956,
           "rotation": 0,
           "target": "2-3-crowning-with-thorns"
+        },
+        {
+          "yaw": -2.359248884247041,
+          "pitch": -0.08920004054481012,
+          "rotation": 0,
+          "target": "0-1-the-agony-in-the-garden"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.019879467823962216,
-          "pitch": -0.3777665840187243,
+          "yaw": -0.05710356659230875,
+          "pitch": -0.36394443518712016,
           "title": "Jesus Before Pilate",
-          "text": "Very early in the morning, the chief priests, with the elders, the teachers of the law and the whole Sanhedrin, made their plans. So they bound Jesus, led him away and handed him over to Pilate.<div><br></div><div>“Are you the king of the Jews?” asked Pilate.</div><div><br></div><div>“You have said so,” Jesus replied.</div><div><br></div><div>The chief priests accused him of many things. So again Pilate asked him, “Aren’t you going to answer? See how many things they are accusing you of.”</div><div><br></div><div>But Jesus still made no reply, and Pilate was amazed.</div><div><br></div><div>Now it was the custom at the festival to release a prisoner whom the people requested. A man called Barabbas was in prison with the insurrectionists who had committed murder in the uprising. The crowd came up and asked Pilate to do for them what he usually did.</div><div><br></div><div>“Do you want me to release to you the king of the Jews?” asked Pilate, 10 knowing it was out of self-interest that the chief priests had handed Jesus over to him. 11 But the chief priests stirred up the crowd to have Pilate release Barabbas instead.</div><div><br></div><div>“What shall I do, then, with the one you call the king of the Jews?” Pilate asked them.</div><div><br></div><div>“Crucify him!” they shouted.</div><div><br></div><div>“Why? What crime has he committed?” asked Pilate.</div><div><br></div><div>But they shouted all the louder, “Crucify him!”</div><div><br></div><div>Wanting to satisfy the crowd, Pilate released Barabbas to them. He had Jesus flogged, and handed him over to be crucified.</div><div>(Mark 15:1-15)</div>"
+          "text": "Very early in the morning, the chief priests, with the elders, the teachers of the law and the whole Sanhedrin, made their plans. So they bound Jesus, led him away and handed him over to Pilate.<div><br></div><div>“Are you the king of the Jews?” asked Pilate.</div><div><br></div><div>“You have said so,” Jesus replied.</div><div><br></div><div>The chief priests accused him of many things. So again Pilate asked him, “Aren’t you going to answer? See how many things they are accusing you of.”</div><div><br></div><div>But Jesus still made no reply, and Pilate was amazed.</div><div><br></div><div>Now it was the custom at the festival to release a prisoner whom the people requested. A man called Barabbas was in prison with the insurrectionists who had committed murder in the uprising. The crowd came up and asked Pilate to do for them what he usually did.</div><div><br></div><div>“Do you want me to release to you the king of the Jews?” asked Pilate, knowing it was out of self-interest that the chief priests had handed Jesus over to him. But the chief priests stirred up the crowd to have Pilate release Barabbas instead.</div><div><br></div><div>“What shall I do, then, with the one you call the king of the Jews?” Pilate asked them.</div><div><br></div><div>“Crucify him!” they shouted.</div><div><br></div><div>“Why? What crime has he committed?” asked Pilate.</div><div><br></div><div>But they shouted all the louder, “Crucify him!”</div><div><br></div><div>Wanting to satisfy the crowd, Pilate released Barabbas to them. He had Jesus flogged, and handed him over to be crucified.</div><div>(Mark 15:1-15)</div>"
         }
       ]
     },
@@ -132,22 +138,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.7469860510475037,
-          "pitch": 0.004887940589071604,
+          "yaw": -0.7715158570633243,
+          "pitch": -0.014325492505331994,
           "rotation": 0,
           "target": "3-4-the-carrying-of-the-cross"
         },
         {
-          "yaw": -3.1375585321533084,
-          "pitch": -0.10265101022275047,
+          "yaw": 3.1311148115449337,
+          "pitch": -0.12542260296349816,
           "rotation": 0,
           "target": "1-2-the-scourging-at-the-pillar"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.7715165002859088,
-          "pitch": -0.2086324739733545,
+          "yaw": -0.7861387385569572,
+          "pitch": -0.2237004379016856,
           "title": "The Soldiers Mock Jesus",
           "text": "The soldiers led Jesus away into the palace (that is, the Praetorium) and called together the whole company of soldiers. They put a purple robe on him, then twisted together a crown of thorns and set it on him. And they began to call out to him, “Hail, king of the Jews!” Again and again they struck him on the head with a staff and spit on him. Falling on their knees, they paid homage to him. And when they had mocked him, they took off the purple robe and put his own clothes on him. Then they led him out to crucify him.<div>(Mark 15:16-19)</div>"
         }
@@ -187,24 +193,24 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.7081349421989067,
-          "pitch": -0.4208162339532482,
+          "yaw": 0.6923570663616143,
+          "pitch": -0.36635344495810784,
           "rotation": 0,
           "target": "4-5-the-crucifixion-of-jesus"
         },
         {
-          "yaw": 3.100161984897351,
-          "pitch": 0.29395046683293025,
+          "yaw": 3.1237215798272615,
+          "pitch": 0.2906576012676574,
           "rotation": 0,
           "target": "2-3-crowning-with-thorns"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.6753313773404095,
-          "pitch": -0.6087821712042061,
-          "title": "Title - Confirm",
-          "text": "As the soldiers led him away, they seized Simon from Cyrene, who was on his way in from the country, and put the cross on him and made him carry it behind Jesus. A large number of people followed him, including women who mourned and wailed for him. Jesus turned and said to them, “Daughters of Jerusalem, do not weep for me; weep for yourselves and for your children. For the time will come when you will say, ‘Blessed are the childless women, the wombs that never bore and the breasts that never nursed!’ Then<div><br></div><div>“‘they will say to the mountains, “Fall on us!” and to the hills, “Cover us!”’</div><div><br></div><div>For if people do these things when the tree is green, what will happen when it is dry?”</div><div>(Luke 23:26-31)</div>"
+          "yaw": 0.5806516692393355,
+          "pitch": -0.6156900716142921,
+          "title": "The Carrying of the Cross",
+          "text": "As the soldiers led him away, they seized Simon from Cyrene, who was on his way in from the country, and put the cross on him and made him carry it behind Jesus. A large number of people followed him, including women who mourned and wailed for him. Jesus turned and said to them, “Daughters of Jerusalem, do not weep for me; weep for yourselves and for your children. For the time will come when you will say, ‘Blessed are the childless women, the wombs that never bore and the breasts that never nursed!’ Then<div><br></div><div>“‘they will say to the mountains, “Fall on us!” and to the hills, “Cover us!”’</div><div><br></div><div>For if people do these things when the tree is green, what will happen when it is dry?”</div><div><br></div><div>(Luke 23:26-31)</div>"
         }
       ]
     },
@@ -242,18 +248,18 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.8945447169091203,
-          "pitch": 0.36687116965749134,
+          "yaw": -1.8217221374557617,
+          "pitch": 0.39267334774008056,
           "rotation": 0,
           "target": "3-4-the-carrying-of-the-cross"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.6319278821126435,
-          "pitch": -0.592874731910225,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 0.6048974703371073,
+          "pitch": -0.5732276376080705,
+          "title": "The Crucifixion of Jesus",
+          "text": "Finally Pilate handed him over to them to be crucified.<div><div><br></div><div><b>The Crucifixion of Jesus</b><br><div><br></div><div>So the soldiers took charge of Jesus. Carrying his own cross, he went out to the place of the Skull (which in Aramaic is called Golgotha). There they crucified him, and with him two others—one on each side and Jesus in the middle.</div></div><div><br></div><div>Pilate had a notice prepared and fastened to the cross. It read: JESUS OF NAZARETH, THE KING OF THE JEWS. Many of the Jews read this sign, for the place where Jesus was crucified was near the city, and the sign was written in Aramaic, Latin and Greek. The chief priests of the Jews protested to Pilate, “Do not write ‘The King of the Jews,’ but that this man claimed to be king of the Jews.”</div><div><br></div><div>Pilate answered, “What I have written, I have written.”</div><div><br></div><div>When the soldiers crucified Jesus, they took his clothes, dividing them into four shares, one for each of them, with the undergarment remaining. This garment was seamless, woven in one piece from top to bottom.</div><div><br></div><div>“Let’s not tear it,” they said to one another. “Let’s decide by lot who will get it.”</div><div><br></div><div>This happened that the scripture might be fulfilled that said,</div><div><br></div><div>“They divided my clothes among them and cast lots for my garment.”</div><div><br></div><div>So this is what the soldiers did.</div><div><br></div><div>Near the cross of Jesus stood his mother, his mother’s sister, Mary the wife of Clopas, and Mary Magdalene. When Jesus saw his mother there, and the disciple whom he loved standing nearby, he said to her, “Woman,[b] here is your son,” and to the disciple, “Here is your mother.” From that time on, this disciple took her into his home.</div><div><br></div><div><b>The Death of Jesus</b></div><div><br></div><div>Later, knowing that everything had now been finished, and so that Scripture would be fulfilled, Jesus said, “I am thirsty.” A jar of wine vinegar was there, so they soaked a sponge in it, put the sponge on a stalk of the hyssop plant, and lifted it to Jesus’ lips. When he had received the drink, Jesus said, “It is finished.” With that, he bowed his head and gave up his spirit.</div></div><div>(John 19:16-30)</div>"
         }
       ]
     }
